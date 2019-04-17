24bc8b1956198025c302467dee10d7d7926060a0
xiaofeixiong <1749239438@qq.com>
2019-04-17 11:04:33 +0800

    fix: 请求接口封装

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
-import axios from '@/utils/http.js'
+import http from '@/utils/http.js'
 const BASE_URL = 'http://10.2.0.216:8080'
 
 export function getBatchList () { // 查询所有批号
-  return axios.post(`${BASE_URL}/mes/warehouseInfo/storage/getBatchList`)
+  return http.post(`${BASE_URL}/mes/warehouseInfo/storage/getBatchList`)
 }
--- src/utils/http.js
+++ src/utils/http.js
@@ -1,23 +1,28 @@
 import axios from 'axios'
 
-// 设置默认请求token
-axios.defaults.headers.common['Authorization'] = window.sessionStorage.token ? window.sessionStorage.token : ''
-
-// 大多数的web服务器只能识别form的post的请求，即请求头Content-Type为'application/x-www-form-urlencoded'
-// axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.headers.post['Content-Type'] = 'application/json;charset=UTF-8'
+let http = axios.create({
+  headers: {
+    // 设置默认请求token
+    'Authorization': window.sessionStorage.token ? window.sessionStorage.token : '',
+    // 大多数的web服务器只能识别form的post的请求，即请求头Content-Type为'application/x-www-form-urlencoded'
+    post: {
+      'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+    }
+  }
+})
 
 // 添加请求拦截器
-axios.interceptors.request.use(function (request) {
+http.interceptors.request.use(function (config) {
+  // console.log(config.headers, '请求配置')
   // 在发送请求之前做些什么
-  return request
+  return config
 }, function (error) {
   // 对请求错误做些什么
   return Promise.reject(error)
 })
 
 // 添加响应拦截器
-axios.interceptors.response.use(function (response) {
+http.interceptors.response.use(function (response) {
   // 对响应数据做点什么
   return response && response.data
 }, function (error) {
@@ -25,4 +30,4 @@ axios.interceptors.response.use(function (response) {
   return Promise.reject(error)
 })
 
-export default axios
+export default http
