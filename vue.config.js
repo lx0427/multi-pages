2fd6478b96565a188a99f7b25a7657038b398ab7
xiaofeixiong <1749239438@qq.com>
2019-04-17 08:35:15 +0800

    save

--- vue.config.js
+++ vue.config.js
@@ -27,7 +27,7 @@ module.exports = {
     loaderOptions: {
       sass: {
         // @/ 是 src/ 的别名
-        // 所以这里假设你有 `src/variables.scss` 这个文件
+        // 全局引用公用样式
         data: `@import "@/assets/common/reset.scss";`
       },
       'postcss': {
